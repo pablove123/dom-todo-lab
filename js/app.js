210dd22bb1dc3f268ced33bbfd0a00c093473a9f
Pablo Villedae <pablov.e12345@gmail.com>
2022-12-06 16:33:06 -0800

    all completed

--- js/app.js
+++ js/app.js
@@ -1,1 +1,11 @@
-console.log("slay")
+const inp = document.querySelector("input")
+const btn = document.querySelector("button")
+const ul = document.querySelector("ul")
+
+btn.addEventListener("click", function(text){
+  const li = document.createElement("li")
+  const input = document.querySelector("input")
+  li.textContent = input.value
+   document.querySelector("ul").appendChild(li)
+   inp.value = " "
+})
